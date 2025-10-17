205a2a1c7856da3135b0ddd27f4214b58adb6648
Dominik Böhler <dominik.boehler@gmx.net>
2025-10-17 16:45:09 +0200

    error!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, class!");
+pub fn hello() -> String {
+    "Hello, World!".to_string()
 }
+
+fn main() {
+    println!("{}", hello());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn hello_returns_correct_string() {
+        assert_eq!(hello(), "Hello, world!");
+    }
+}
+
