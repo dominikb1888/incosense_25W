4b38597b06ebdc5fd3ed2a21c428f83c3baf40e3
Dominik Böhler <dominik.boehler@gmx.net>
2025-10-24 16:36:00 +0200

    healthcheck route created

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-pub fn hello() -> String {
-    "Hello, world!".to_string()
+use axum::{Router, routing::get};
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/healthcheck", get(healthcheck));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
 
-fn main() {
-    println!("{}", hello());
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn hello_returns_correct_string() {
-        assert_eq!(hello(), "Hello, world!");
-    }
+// Build a function that returns an HTTP Response 200 OK with an empty body
+async fn healthcheck() -> &'static str {
+    ""
 }
