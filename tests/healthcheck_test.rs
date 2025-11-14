a362cf982d22fb15b73593852ac084b2d93b3772
Dominik Böhler <dominik.boehler@gmx.net>
2025-11-14 19:50:14 +0100

    database tests done

--- tests/healthcheck_test.rs
+++ tests/healthcheck_test.rs
@@ -1,15 +1,15 @@
-use axum::body;
 use hyper::StatusCode;
 use incosense::build_router;
 use sqlx::PgPool;
 use tokio::net::TcpListener;
 use tokio::task::JoinHandle;
 
+use incosense::AppState;
 use incosense::configuration::get_configuration;
 
 #[tokio::test]
 async fn healthcheck_works() {
-    let (base_url, server_handle) = spawn_app().await;
+    let (base_url, server_handle, _connection_pool) = spawn_app().await;
 
     let response = reqwest::get(format!("{base_url}/healthcheck"))
         .await
@@ -23,7 +23,7 @@ async fn healthcheck_works() {
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    let (base_url, server_handle) = spawn_app().await;
+    let (base_url, server_handle, connection_pool) = spawn_app().await;
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=Ursula_le_guin%40gmail.com";
@@ -38,12 +38,20 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     assert_eq!(StatusCode::OK, response.status());
 
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&connection_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "Ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
+
     server_handle.abort();
 }
 
 #[tokio::test]
 async fn subscribe_returns_a_422_when_data_is_missing() {
-    let (base_url, server_handle) = spawn_app().await;
+    let (base_url, server_handle, _connection_pool) = spawn_app().await;
 
     let client = reqwest::Client::new();
     let test_cases = vec![
@@ -74,7 +82,7 @@ async fn subscribe_returns_a_422_when_data_is_missing() {
 
 #[tokio::test]
 async fn test_non_utf8_form_rejected() {
-    let (base_url, server_handle) = spawn_app().await;
+    let (base_url, server_handle, _connection_pool) = spawn_app().await;
 
     let invalid_payloads: &[&[u8]] = &[
         b"name=H\xE4llo&email=W\xF6rld", // ISO-8859-1 ä ö
@@ -107,7 +115,7 @@ async fn test_non_utf8_form_rejected() {
     server_handle.abort();
 }
 
-pub async fn spawn_app() -> (String, JoinHandle<()>) {
+pub async fn spawn_app() -> (String, JoinHandle<()>, PgPool) {
     let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
     let port = listener.local_addr().unwrap().port();
 
@@ -117,11 +125,17 @@ pub async fn spawn_app() -> (String, JoinHandle<()>) {
         .await
         .expect("Failed to connect to Postgres.");
 
-    let app = build_router(connection_pool);
+    let app = build_router(AppState {
+        db: connection_pool.clone(),
+    });
 
     let server_handle = tokio::spawn(async move {
         axum::serve(listener, app).await.unwrap();
     });
 
-    (format!("http://127.0.0.1:{port}"), server_handle)
+    (
+        format!("http://127.0.0.1:{port}"),
+        server_handle,
+        connection_pool,
+    )
 }
